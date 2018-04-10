4b5d76d81cbbab7e570a6ab3b26467550158230a
Reynoso Matías <soloamimepasa@hotmail.com>
2018-04-10 20:06:07 -0300

    Update tp3.sql

--- tp3.sql
+++ tp3.sql
@@ -7,7 +7,7 @@ id int auto_increment primary key,
 mesa varchar (50) default null);
 
 create table ganador (
-id int not null primary key,
+id int not null,
 jugador varchar (50) not null,
 cartas int not null,
 constraint fk_id_juego foreign key (id) references juegos(id));
